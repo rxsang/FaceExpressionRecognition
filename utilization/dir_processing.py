9ff53d46f8203d26d860f1dba136aa6d00064655
rxsang <blackashely1989@gmail.com>
2014-07-25 11:21:49 +0800

    add draw landmarks on sequence

--- utilization/dir_processing.py
+++ utilization/dir_processing.py
@@ -68,19 +68,22 @@ class DirProcessing:
         return landmarks_url
 
     @staticmethod
-    def get_all_imgs_url_from_sequence(person_id, perform_id):
+    def get_all_img_urls_from_sequence(person_id, perform_id):
         person_sid = 'S' + person_id.zfill(3)
         perform_sid = perform_id.zfill(3)
         
         url_list = []
+        index_id = 1
         while True:
+            index_sid = str(index_id).zfill(8)
             img_folder = "{}/{}/{}/{}".format(DirProcessing.dataset_root, DirProcessing.image_folder, person_sid, perform_sid)
-            img_name = "{}_{}_{}.png".format(person_sid, perform_sid, 1)
-            img_url = img_folder + '/' + 'img_name'
+            img_name = "{}_{}_{}.png".format(person_sid, perform_sid, index_sid)
+            img_url = img_folder + '/' + img_name
+            print img_url
             if not os.path.isfile(img_url): 
                 break
             url_list.append(img_url)
-
+            index_id = index_id + 1
         return url_list
 
 
--- visualization/visualization.py
+++ visualization/visualization.py
@@ -14,7 +14,10 @@ class Visualization:
         cv2.destroyAllWindows()
 
     @staticmethod
-    def draw_landmarks_on_img(img_url, landmarks_url):
+    def draw_landmarks_on_img(img_url):
+        from dir_processing import DirProcessing
+        landmarks_url = DirProcessing.generate_landmarks_url_from_img_url(img_url)
+
         from file_op import FileOp
         img = FileOp.read_img_file(img_url)
         landmarks = FileOp.read_landmarks_file(landmarks_url)
@@ -29,6 +32,34 @@ class Visualization:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
+    @staticmethod
+    def draw_landmarks_on_sequence(img_urls):
+        from dir_processing import DirProcessing
+        landmarks_urls = []
+        for i in xrange(0, len(img_urls)):
+            img_url = img_urls[i]
+            landmarks_url = DirProcessing.generate_landmarks_url_from_img_url(img_url)
+            landmarks_urls.append(landmarks_url)
+
+        from file_op import FileOp
+        for i in xrange(0, len(img_urls)):
+            img_url = img_urls[i]
+            landmarks_url = landmarks_urls[i]
+            img = FileOp.read_img_file(img_url)
+            landmarks = FileOp.read_landmarks_file(landmarks_url)
+
+            for i in xrange(0, landmarks.shape[0]):   # for every point
+                loc = landmarks[i, :]
+                x = int(round(loc[0]))
+                y = int(round(loc[1]))
+                cv2.circle(img, (x, y), 1, 255)
+            
+            print img_url
+            cv2.imshow('image' + str(i), img)
+            cv2.waitKey(0)
+        
+        cv2.destroyAllWindows()
+                                                                                        
 
     @staticmethod
     def test():
@@ -36,9 +67,12 @@ class Visualization:
         sys.path.append(lib_path)
 
         from dir_processing import DirProcessing
-        img_url = DirProcessing.generate_img_url('10','2','1')
-        landmarks_url = DirProcessing.generate_landmarks_url_from_img_url(img_url)
-        Visualization.draw_landmarks_on_img(img_url, landmarks_url)
+
+        img_urls = DirProcessing.get_all_img_urls_from_sequence('79','1')
+        print img_urls
+        Visualization.draw_landmarks_on_sequence(img_urls)
+#        img_url = DirProcessing.generate_img_url('10','2','1')
+#        Visualization.draw_landmarks_on_img(img_url)
 
 ####################################3
     
